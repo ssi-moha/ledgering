5927d313e04f6be552da1ddee98d34f454532972
Malek Rezgui <m.rezgui@outlook.fr>
2022-07-05 12:00:09 +0200

    feat: add fund style

--- front/src/ui/components/panels/Sender.tsx
+++ front/src/ui/components/panels/Sender.tsx
@@ -1,10 +1,16 @@
 import { Box, Button } from "@chakra-ui/react";
-import { useWeb3React } from "@web3-react/core";
-import { ethers } from "ethers";
 import { useSelector } from "react-redux";
 import useGetBalances from "../../../hooks/useGetBalances";
 import { sendEther } from "../../../store/slices/ether";
 import { RootState, useAppDispatch } from "../../../store/store";
+
+const tokenIcon = {
+  ETH: "eth",
+  BUSD: "busd",
+  USDT: "usdt",
+  USDC: "usdc",
+  BTC: "btc",
+};
 
 const Sender = () => {
   const balances = useGetBalances();
@@ -13,20 +19,54 @@ const Sender = () => {
 
   const handleClick = () => {
     if (ledger) dispatch(sendEther(ledger));
+    console.log("clicked");
   };
 
   return (
-    <Box>
-      <div>Send your Funds</div>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+    >
+      <Box mt="4">
+        <strong> Send your Funds </strong>
+      </Box>
 
       <div>
-        {balances.map((token) => (
-          <div>
-            {token.symbol}: {token.balance}
-          </div>
-        ))}
+        {balances.map((token) => {
+          const { symbol, balance } = token;
+
+          return (
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              flexDirection="column"
+              mt={6}
+              p={3}
+              borderWidth={1}
+              borderColor={"white"}
+            >
+              <Box display={"flex"} p={3} borderWidth={1} borderColor={"white"}>
+                <img width="24px" src={`${symbol}.svg`} alt={symbol} />
+                <span style={{ margin: "auto", marginLeft: "4px" }}>
+                  {balance}
+                </span>
+                <span style={{ margin: "auto", marginLeft: "4px" }}>
+                  {symbol}
+                </span>
+              </Box>
+
+              <Box mt={4}>
+                <Button bg="#f1f1f1" color={"black"} onClick={handleClick}>
+                  Send
+                </Button>
+              </Box>
+            </Box>
+          );
+        })}
       </div>
-      <Button onClick={handleClick}>TEST</Button>
     </Box>
   );
 };
