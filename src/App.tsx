50741cd56c6cac1a3618ee46893d72a4628b7f15
Sinane <sin.sim@outlook.fr>
2022-07-25 17:19:18 +0200

    refactor: delete unused package and useless wording

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import { Box, VStack } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
 
 import Header from "./ui/components/Header";
-import Stepper from "./ui/components/Stepper";
+import Main from "./ui/components/Main";
 
 function App() {
   return (
     <VStack py="2">
       <Header type="h1" title="Ledgering your crypto" />
 
-      <Box w="95%" px="6" py="4">
-        Pourquoi l'utiliser? On sait depuis longtemps que travailler avec du
-        texte lisible et contenant du sens est source de distractions, et
-        empêche de se concentrer sur la mise en page elle-même. L'avantage du
-        Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.'
-      </Box>
-
-      <Box w="full" margin="auto" mt={4}>
-        <Stepper />
-      </Box>
+      <Main />
     </VStack>
   );
 }
---
+++ src/ui/components/Main.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import Stepper from "./Stepper";
+
+const Main = () => {
+  return (
+    <Box as="main" w="full" margin="auto">
+      <Stepper />
+    </Box>
+  );
+};
+
+export default Main;
